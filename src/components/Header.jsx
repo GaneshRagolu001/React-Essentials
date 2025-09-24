e1e08c8a7ae8da2c2c693411b77a55203e966221
Ganesh <ganeshragolu001@gmail.com>
2025-09-24 16:31:13 +0530

    changed header description with dynamic values

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Header.css";
+import headerimg from "../assets/react-core-concepts.png";
+
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function generateRandom(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+
 const Header = () => {
+  const description = reactDescriptions[generateRandom(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={headerimg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+       {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
